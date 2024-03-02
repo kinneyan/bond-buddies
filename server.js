10fa18b06388fb11e0576bb5fd482d66b6bf5e7e
Andrew Kinney <drewkinney03@gmail.com>
2024-03-01 23:54:08 -0500

    Fix typo

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ if (process.env.NODE_ENV === 'production')
 
     app.get('*', (req, res) =>
     {
-        res.sendFile(path.resulve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
