73506fdb1bd26bcdea2d8e0eae35703a90d63cf1
Andrew Kinney <drewkinney03@gmail.com>
2024-03-03 22:36:37 -0500

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App()
 {
     return (
-        <HomePage/>
+        <Router>
+            <Routes>
+                <Route exact path='/' element={ <HomePage /> } />
+                <Route path='/login' element={ <LoginPage /> } />
+                <Route path='/register' element={ <RegisterPage /> } />
+            </Routes>
+        </Router>
     );
 }
 
