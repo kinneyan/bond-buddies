1837c80f4026c47710f69250ea991fc9dd58fa67
Andrew Kinney <drewkinney03@gmail.com>
2024-03-04 23:39:13 -0500

    Move authorization to middleware

--- controllers/removeFriend.js
+++ controllers/removeFriend.js
@@ -1,25 +1,10 @@
-const { verifyJWT } = require('../utils/auth');
-
 const removeFriend = (async (req, res, next) => 
 {
     // header: auth token
     // body: friend username
     // response: error
 
-    // initialize response object
-    let ret = {};
-    ret.error = '';
-
-
-    const token = req.get('Authorization');
-    if (token === undefined)
-    {
-        ret.error = 'Could not authenticate request.';
-        res.status(401).json(ret);
-        return;
-    }
-
-    res.status(200).json({});
+    res.status(200).json(res.locals.ret);
 });
 
 module.exports = { removeFriend };
