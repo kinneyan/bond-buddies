a616805446501cb74a6a1ebf29fffccee52cc178
Andrew Kinney <drewkinney03@gmail.com>
2024-03-12 17:30:44 -0400

    Remove search friend
    Exists in #24. Not relevant in this PR.

--- routes/friends.js
+++ routes/friends.js
@@ -5,14 +5,10 @@ const { removeFriend } = require('../controllers/removeFriend');
 
 // get middleware
 const { authenticate } = require('../middleware/auth');
-
-const { searchFriends } = require('../controllers/searchFriends');
 const { addFriend } = require('../controllers/addFriend');
 
 router.post('/remove', authenticate);
 router.post('/remove', removeFriend);
-router.post('/search', authenticate);
-router.post('/search', searchFriends);
 router.post('/add', authenticate);
 router.post('/add', addFriend);
 
