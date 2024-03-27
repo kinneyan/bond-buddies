e463bead9452d1deb62a13a668308a83e7f0c81a
Andrew Kinney <drewkinney03@gmail.com>
2024-03-26 21:34:35 -0400

    Add interpretResults file

---
+++ controllers/interpretResults.js
@@ -0,0 +1,18 @@
+const getResults = (async (req, res, next) => 
+{
+    // header: auth token
+    // response: interpreted assessment results
+
+    try 
+    {
+
+    }
+    catch (e)
+    {
+        res.locals.ret.error = 'Could not interpret assessment results.';
+        res.status(500).json(res.locals.ret);
+        return;
+    }
+});
+
+module.exports = { getResults };
