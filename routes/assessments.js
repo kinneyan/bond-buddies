fef380a31ea761c2761c1a998ffd9e4c5867e207
Andrew Kinney <drewkinney03@gmail.com>
2024-03-26 21:27:29 -0400

    Initialize getResults endpoint

--- routes/assessments.js
+++ routes/assessments.js
@@ -4,8 +4,12 @@ const router = express.Router();
 // get middleware
 const { authenticate } = require('../middleware/auth');
 const { updateAssessment } = require('../controllers/updateAssessments');
+const { getResults } = require('../controllers/interpretResults');
 
 router.post('/update', authenticate);
 router.post('/update', updateAssessment);
+
+router.get('/get', authenticate);
+router.get('/get', getResults);
 
 module.exports = router;
