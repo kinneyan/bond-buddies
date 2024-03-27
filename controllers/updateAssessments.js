bb2e87ae3cd8dcbe3be43975d79d87e26b804dab
Andrew Kinney <drewkinney03@gmail.com>
2024-03-26 21:02:12 -0400

    Implement updateAssessment endpoint

--- controllers/updateAssessments.js
+++ controllers/updateAssessments.js
@@ -1,6 +1,73 @@
+const { getMongoClient } = require('../utils/database');
+
 const updateAssessment = (async (req, res, next) => 
 {
+    // header: auth token
+    // body: assessment (int), responses (int array)
+    // response: error
 
+    // read body
+    let assessment = -1;
+    let responseArray = [];
+    try
+    {
+        const { assessmentCode, responses } = req.body;
+        
+        switch (assessmentCode)
+        {
+            case 0:
+                assessment = 'Personality';
+                break;
+            case 1:
+                assessment = 'DISC';
+                break;
+            case 2:
+                assessment = 'Friendship';
+                break;
+            default:
+                throw new Error();
+        }
+
+        if (responses.length != 20) throw new error();
+        responseArray = responses;
+    }
+    catch (e)
+    {
+        res.locals.ret.error = 'Bad request. Missing or invalid information.';
+        res.status(400).json(res.locals.ret);
+        return;
+    }
+
+    try
+    {
+        const client = getMongoClient();
+        client.connect();
+        const db = client.db();
+
+        const update = await db.collection(assessment).updateOne(
+            { login: res.locals.token.login },
+            { $set: { responses: responseArray }},
+            { upsert: true }
+        );
+
+        if (update.modifiedCount < 1)
+        {
+            res.locals.ret.error = 'Server failed to update responses.';
+            res.status(409).json(res.locals.ret);
+            return;
+        }
+
+        res.locals.ret.error = '';
+        res.status(200).json(res.locals.ret);
+        return;
+    }
+    catch (e)
+    {
+        console.log(e);
+        res.locals.ret.error = 'Encoutered an error while updating assessment responses.';
+        res.status(500).json(res.locals.ret);
+        return;
+    }
 });
 
 module.exports = { updateAssessment };
