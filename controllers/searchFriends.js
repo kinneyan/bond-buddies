7cad815c12e5facac77972bd1505db0353185938
Victoria Oppel <victoriaoppel@blue>
2024-03-27 11:02:25 -0400

    added search by first name, last name, username, or email

--- controllers/searchFriends.js
+++ controllers/searchFriends.js
@@ -1,7 +1,6 @@
 const { getMongoClient } = require('../utils/database');
 
-const searchFriends = (async (req, res, next) => 
-{
+const searchFriends = async (req, res, next) => {
     // incoming: userId, search
 
     // outgoing: results or error
@@ -10,64 +9,65 @@ const searchFriends = (async (req, res, next) => 
     let ret = {};
     ret.error = '';
 
-    try
-    {
-        const {search } = req.body;
+    try {
+        const { search } = req.body;
         _search = search.trim();
         if (_search === '') throw new Error();
-    }    
-    catch (e)
-    {
+    } catch (e) {
         res.locals.ret.error = 'Bad request syntax. Missing or incorrect information.'
         res.status(400).json(res.locals.ret);
         return;
     }
-    try{
+
+    try {
         const client = getMongoClient();
-        client.connect();
+        await client.connect();
         const db = client.db();
-        // check if search exists as a user
-        const users = await db.collection('Users').find({ Login: _search }).toArray();
-        if (users.length < 1)
-        {
+
+        // check if search exists as a user by first name, last name, email, or username (case-insensitive)
+        const users = await db.collection('Users').find({
+            $or: [
+                { FirstName: { $regex: new RegExp(_search, 'i') } }, // Case-insensitive regex for first name
+                { LastName: { $regex: new RegExp(_search, 'i') } },  // Case-insensitive regex for last name
+                { Email: { $regex: new RegExp(_search, 'i') } },     // Case-insensitive regex for email
+                { Login: { $regex: new RegExp(_search, 'i') } }      // Case-insensitive regex for username
+            ]
+        }).toArray();
+
+        if (users.length < 1) {
             res.locals.ret.error = _search + ' is not a user.';
             res.status(409).json(res.locals.ret);
             return;
         }
-        
+
         let ids = [users[0]._id.toString(), res.locals.token.id];
-        ids.sort((a, b) =>
-        {
+        ids.sort((a, b) => {
             return a.localeCompare(b);
         });
 
         const requestBody = { user1: ids[0], user2: ids[1] };
-        
+
         // check if there is an existing relationship
         const relationships = await db.collection('Relationships').find(requestBody).toArray();
-        if (relationships.length < 1)
-        {
+        if (relationships.length < 1) {
             res.locals.ret.error = _search + ' is not a friend.';
             res.status(409).json(res.locals.ret);
             return;
-        }
-        else
-        {
+        } else {
             ret.firstName = users[0].FirstName;
-            ret.lastname = users[0].LastName;
+            ret.lastName = users[0].LastName;
             ret.relationshipType = relationships[0].RelationshipType;
             ret.email = users[0].Email;
-    
+
         }
 
-    }
-    catch(e){
+    } catch (e) {
         res.locals.ret.error = 'Encountered an error while searching for friend.';
         res.status(500).json(res.locals.ret);
         return;
     }
     res.status(200).json(ret);
     return;
-    
-});
+};
+
 module.exports = { searchFriends };
