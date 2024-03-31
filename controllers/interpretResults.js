b9ae5ae0e3e868a8f16c66415099d06f694c39f3
Andrew Kinney <drewkinney03@gmail.com>
2024-03-30 23:55:24 -0400

    Get BMPT results

--- controllers/interpretResults.js
+++ controllers/interpretResults.js
@@ -1,3 +1,5 @@
+const { getMongoClient } = require('../utils/database');
+
 const getResults = (async (req, res, next) => 
 {
     // header: auth token
@@ -5,10 +7,24 @@ const getResults = (async (req, res, next) =>
 
     try 
     {
-        
+        const client = getMongoClient();
+        client.connect();
+        const db = client.db();
+
+        let results = {};
+
+        // check for personality test
+        const personality = await db.collection('Personality').find({ login: res.locals.token.login }).toArray();
+        if (personality.length < 1) throw new Error();
+
+        results.error = '';
+        results.personality = personality[0].result;
+        res.status(200).json(results);
+        return;
     }
     catch (e)
     {
+        console.log(e);
         res.locals.ret.error = 'Could not interpret assessment results.';
         res.status(500).json(res.locals.ret);
         return;
