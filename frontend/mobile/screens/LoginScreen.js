f235ed318fb8ff71ff37e1724f1f10aebcf42a52
yasminie <99419128+yasminie@users.noreply.github.com>
2024-04-04 02:58:04 -0400

    Add files via upload

--- frontend/mobile/screens/LoginScreen.js
+++ frontend/mobile/screens/LoginScreen.js
@@ -3,6 +3,7 @@ import { View, Dimensions, StyleSheet, Text, TextInput, TouchableOpacity } from 
 import { useNavigation } from '@react-navigation/native';
 import { Rect, CornerPathEffect} from "@shopify/react-native-skia";
 import BorderGradient from '../components/BorderGradient'; 
+import { API_URL } from '../components/ApiAddress';
 
 export default function LoginScreen() {
 
@@ -11,6 +12,82 @@ export default function LoginScreen() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const [errors, setErrors] = useState({
+        username: '',
+        password: '',
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const validateUsername = (username) => {
+
+        if(!username.trim()) {
+            return 'Username cannot be blank';
+        } 
+        else{
+            return '';
+        }
+    }; 
+
+    const validatePassword = (password) => {
+
+        if(!password.trim()) {
+            return 'Password cannot be blank';
+        } 
+        else{
+            return '';
+        }
+    }; 
+
+    const handleLogin = async () => {
+
+        const usernameError = validateUsername(username);
+        const passwordError = validatePassword(password);
+
+        setErrors({ ...errors, username: usernameError, password: passwordError }); 
+
+        if(usernameError || passwordError){ 
+            return;
+        }
+
+        const obj = {
+            username,
+            password,
+        };
+
+        try{
+
+            const response = await fetch(API_URL + '/user/login', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(obj)
+            });
+
+            const res = await response.json();
+
+            if(res.error){
+                setErrorMessage("Username or password is incorrect");
+            } 
+            else{
+                console.log("Login successful!");
+                navigation.navigate('UserHome', { username });
+            }
+        }catch(error){
+            console.error("Error during login:", error);
+        }
+
+    };
+    
+    const navigateToForgotPassword = () => {
+        navigation.navigate('Forgot'); 
+    };
+
+    const navigateToRegister = () => {
+        navigation.navigate('Register');
+    };
 
     const styles = StyleSheet.create({
         container: {
@@ -92,25 +169,18 @@ export default function LoginScreen() {
             color: 'blue',
             textDecorationLine: 'none',
         },
+        ErrorText: {
+            textAlign: 'center',
+            fontFamily: 'Roboto Regular',
+            fontSize: 16,
+            marginTop: 30,
+            color: 'red',
+        },
+        inputError: {
+            borderColor: 'red',
+            borderWidth: 2,
+        },
     });
-
-    const handleLogin = () => {
-        // login logic, verify credentials with backend
-        if(username.trim() !== '' && password.trim() !== '') {
-            navigation.navigate('UserHome');
-        } 
-        else{
-            console.log('Invalid credentials');
-        }
-    };
-    
-    const navigateToForgotPassword = () => {
-        navigation.navigate('Forgot'); 
-    };
-
-    const navigateToRegister = () => {
-        navigation.navigate('Register');
-    };
 
     return (
         <View style={styles.container}>
@@ -129,8 +199,11 @@ export default function LoginScreen() {
                 <View style={styles.inputContainer}>
                     <Text style={styles.label}>Username</Text>
                     <TextInput
-                        style={styles.input}
-                        onChangeText={text => setUsername(text)}
+                        style={[styles.input, errors.username !== '' && styles.inputError]}
+                        onChangeText={text => {
+                            setUsername(text);
+                            setErrors(prevState => ({ ...prevState, username: validateUsername(text) }));
+                        }}
                         value={username}
                         placeholder="Enter your username"
                     />
@@ -138,8 +211,11 @@ export default function LoginScreen() {
                 <View style={styles.inputContainer}>
                     <Text style={styles.label}>Password</Text>
                     <TextInput
-                        style={styles.input}
-                        onChangeText={text => setPassword(text)}
+                        style={[styles.input, errors.password !== '' && styles.inputError]}
+                        onChangeText={text => {
+                            setPassword(text);
+                            setErrors(prevState => ({ ...prevState, password: validatePassword(text) }));
+                        }}
                         value={password}
                         placeholder="Enter your password"
                         secureTextEntry={true}
@@ -148,6 +224,11 @@ export default function LoginScreen() {
                 <TouchableOpacity style={styles.forgotPasswordContainer} onPress={navigateToForgotPassword}>
                     <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
                 </TouchableOpacity>
+
+                {errors.username !== '' && <Text style={styles.ErrorText}>{errors.username}</Text>}
+                {errors.password !== '' && <Text style={styles.ErrorText}>{errors.password}</Text>}
+                {errorMessage !== '' && <Text style={styles.ErrorText}>{errorMessage}</Text>}
+
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={styles.Loginbutton} onPress={handleLogin}>
                         <Text style={styles.LoginbuttonText}>Log In</Text>
--- frontend/mobile/screens/RegisterScreen.js
+++ frontend/mobile/screens/RegisterScreen.js
@@ -3,6 +3,7 @@ import { View, Dimensions, StyleSheet, Text, TextInput, TouchableOpacity } from 
 import { useNavigation } from '@react-navigation/native';
 import { Rect, CornerPathEffect } from "@shopify/react-native-skia";
 import BorderGradient from '../components/BorderGradient'; 
+import { API_URL } from '../components/ApiAddress';
 
 export default function RegisterScreen() {
 
@@ -19,104 +20,267 @@ export default function RegisterScreen() {
         confirmPassword: ''
     });
 
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    canvas: {
-        position: 'absolute',
-        top: 0,
-    },
-    textContainer: {
-        alignItems: 'center',
-    },
-    text: {
-        fontFamily: 'Omnes Regular',
-        fontSize: 40,
-        color: 'black',
-        marginBottom: 40,
-    },
-    inputContainer: {
-        marginTop: 20,
-    },
-    label: {
-        fontFamily: 'Roboto Regular',
-        fontSize: 20,
-        color: 'black',
-        marginBottom: 10,
-    },
-    input: {
-        width: width * 0.7,
-        height: 40,
-        borderColor: 'black',
-        borderWidth: 2,
-        borderRadius: 10,
-        paddingHorizontal: 10,
-    },
-    buttonContainer: {
-        marginTop: 30,
-        padding: 10,
-    },
-    Loginbutton: {
-        backgroundColor: 'black',
-        padding: 10,
-        marginBottom: 15, 
-        borderWidth: 2,
-        borderRadius: 10,
-        justifyContent: 'center', 
-        color: 'black',
-        width: width * 0.7,
-    },
-    Registerbutton: {
-        borderColor: 'black',
-        backgroundColor: 'white',
-        padding: 10,
-        marginBottom: 30, 
-        borderWidth: 2,
-        borderRadius: 10,
-        justifyContent: 'center', 
-        color: 'black',
-    },
-    LoginbuttonText: {
-        fontFamily: 'Roboto Medium',
-        fontSize: 20,
-        color: 'white',
-        textAlign: 'center',
-    },
-    RegisterbuttonText: {
-        fontFamily: 'Roboto Medium',
-        fontSize: 20,
-        color: 'black',
-        textAlign: 'center',
-    },
+    const [errors, setErrors] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+        passwordsMatchError: ''
     });
 
-    const handleNextStep = (data) => {
-      setUserData(prevState => ({ ...prevState, ...data }));
-      setStep(step + 1);
+    const [attemptedNext, setAttemptedNext] = useState(false); 
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const validateFirstName = (firstName) => {
+
+        if(!firstName.trim()){
+            return 'First name cannot be blank';
+        } 
+        else{
+            const isValid = /^[a-zA-Z]+$/.test(firstName);
+            return isValid ? '' : 'First name must only\ncontain alphabetic characters';
+        }
     };
 
-    const handleRegister = () => {
-      console.log('Registering:', userData);
-      // You can perform registration logic here
+    const validateLastName = (lastName) => {
+
+        if(!lastName.trim()){
+            return 'Last name cannot be blank';
+        } 
+        else{
+            const isValid = /^[a-zA-Z]+$/.test(lastName);
+            return isValid ? '' : 'Last name must only\ncontain alphabetic characters';
+        }
     };
 
+    const validateEmail = (email) => {
+
+        if(!email.trim()){
+            return 'Email cannot be blank';
+        } 
+        else{
+            const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+            return isValid ? '' : 'Email address is invalid';
+        }
+    };
+
+    const validateUsername = (username) => {
+
+        if(!username.trim()){
+            return 'Username cannot be blank';
+        } 
+        else {
+            const containsLetter = /[a-zA-Z]/.test(username);
+            const isValid = /^[a-zA-Z0-9]+$/.test(username);
+            return (isValid && containsLetter) ? '' : 'Username must only contain\nletters and numbers';
+        }
+    };
+
+    const validatePassword = (password) => {
+
+        if(!password.trim()){
+            return 'Password cannot be blank';
+        } 
+        else{
+            const containsUppercase = /[A-Z]/.test(password);
+            const containsNumber = /[0-9]/.test(password);
+            const isValidLength = password.length >= 5;
+            const isValid = /^[a-zA-Z0-9]+$/.test(password);
+            return (isValid && containsUppercase && containsNumber && isValidLength) ? '' : 'Password must contain at least\none uppercase letter, one number,\nand a minimum of 5 characters';
+        }
+    };
+
+    const validateConfirmPassword = (password) => {
+
+        if(!password.trim()) {
+            return 'Confirm password xcannot be blank';
+        } 
+        else{
+            return '';
+        }
+    }; 
+    
+
+    const passwordMatch = (password, confirmPassword) => {
+        return password === confirmPassword ? '' : 'Passwords do not match';
+    }
+
+    const handleNextStep = () => {
+
+        const { firstName, lastName, email } = userData;
+
+        const firstNameError = validateFirstName(firstName);
+        const lastNameError = validateLastName(lastName);
+        const emailError = validateEmail(email);
+
+        setErrors({ ...errors, firstName: firstNameError, lastName: lastNameError, email: emailError });
+
+        if(firstNameError || lastNameError || emailError){
+            setAttemptedNext(true); 
+            return;
+        }
+
+        setStep(step + 1);
+    };
+  
+    const handleRegister = async () => {
+
+        const {firstName, lastName, email, username, password, confirmPassword } = userData;
+
+        const usernameError = validateUsername(username);
+        const passwordError = validatePassword(password);
+        const confirmPasswordError = validateConfirmPassword(confirmPassword);
+        const passwordsMatchError = passwordMatch(password, confirmPassword);
+
+        setErrors({ ...errors, username: usernameError, password: passwordError, confirmPassword: confirmPasswordError }); 
+
+        if(usernameError || passwordError || confirmPasswordError || passwordsMatchError){ 
+            return;
+        }
+        
+            const obj = {
+                firstName,
+                lastName,
+                email,
+                username,
+                password,
+                confirmPassword
+            };
+        
+            try {
+                const response = await fetch(API_URL + '/user/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(obj)
+                });
+        
+                const res = JSON.parse(await response.text());
+        
+                if(res.error === 'Username is already taken.'){
+                    setErrorMessage("Username already exists");
+                    setErrors({ ...errors, username: 'Username already exists' });
+                    return;
+                }
+                else if (res.error) {
+                    console.log("Register failed:", res.error);
+                } 
+                else {
+                    console.log("Register successful!");
+                    navigation.navigate('Login');
+                }
+            } catch (error) {
+                console.error("Error during register:", error);
+            }
+
+    };
+    
     const navigateToLogin = () => {
         navigation.navigate('Login');
     };
+  
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        canvas: {
+            position: 'absolute',
+            top: 0,
+        },
+        textContainer: {
+            alignItems: 'center',
+        },
+        text: {
+            fontFamily: 'Omnes Regular',
+            fontSize: 40,
+            color: 'black',
+            marginBottom: 40,
+        },
+        inputContainer: {
+            marginTop: 20,
+        },
+        label: {
+            fontFamily: 'Roboto Regular',
+            fontSize: 20,
+            color: 'black',
+            marginBottom: 10,
+        },
+        input: {
+            width: width * 0.7,
+            height: 40,
+            borderColor: 'black',
+            borderWidth: 2,
+            borderRadius: 10,
+            paddingHorizontal: 10,
+        },
+        buttonContainer: {
+            marginTop: 30,
+            padding: 10,
+        },
+        Loginbutton: {
+            backgroundColor: 'black',
+            padding: 10,
+            marginBottom: 15, 
+            borderWidth: 2,
+            borderRadius: 10,
+            justifyContent: 'center', 
+            color: 'black',
+            width: width * 0.7,
+        },
+        Registerbutton: {
+            borderColor: 'black',
+            backgroundColor: 'white',
+            padding: 10,
+            marginBottom: 30, 
+            borderWidth: 2,
+            borderRadius: 10,
+            justifyContent: 'center', 
+            color: 'black',
+        },
+        LoginbuttonText: {
+            fontFamily: 'Roboto Medium',
+            fontSize: 20,
+            color: 'white',
+            textAlign: 'center',
+        },
+        RegisterbuttonText: {
+            fontFamily: 'Roboto Medium',
+            fontSize: 20,
+            color: 'black',
+            textAlign: 'center',
+        },
+        ErrorText: {
+            textAlign: 'center',
+            fontFamily: 'Roboto Regular',
+            fontSize: 16,
+            marginTop: 30,
+            color: 'red',
+        },
+        inputError: {
+            borderColor: 'red',
+            borderWidth: 2,
+        },
+    });
 
     const renderStepOne = () => {
-      return (
+
+        return (
           <View style={styles.textContainer}>
               <Text style={styles.text}>Register</Text>
 
               <View style={styles.inputContainer}>
                   <Text style={styles.label}>First Name</Text>
                   <TextInput
-                      style={styles.input}
-                      onChangeText={text => setUserData(prevState => ({ ...prevState, firstName: text }))}
+                      style={[styles.input, (errors.firstName !== '' || (attemptedNext && !userData.firstName.trim())) && styles.inputError]}
+                      onChangeText={text => {
+                          setUserData(prevState => ({ ...prevState, firstName: text }));
+                          setErrors(prevState => ({ ...prevState, firstName: validateFirstName(text) }));
+                      }}
                       value={userData.firstName}
                       placeholder="Enter your first name"
                   />
@@ -125,8 +289,11 @@ export default function RegisterScreen() {
               <View style={styles.inputContainer}>
                   <Text style={styles.label}>Last Name</Text>
                   <TextInput
-                      style={styles.input}
-                      onChangeText={text => setUserData(prevState => ({ ...prevState, lastName: text }))}
+                      style={[styles.input, (errors.lastName !== '' || (attemptedNext && !userData.lastName.trim())) && styles.inputError]}
+                      onChangeText={text => {
+                          setUserData(prevState => ({ ...prevState, lastName: text }));
+                          setErrors(prevState => ({ ...prevState, lastName: validateLastName(text) }));
+                      }}
                       value={userData.lastName}
                       placeholder="Enter your last name"
                   />
@@ -135,12 +302,20 @@ export default function RegisterScreen() {
               <View style={styles.inputContainer}>
                   <Text style={styles.label}>Email</Text>
                   <TextInput
-                      style={styles.input}
-                      onChangeText={text => setUserData(prevState => ({ ...prevState, email: text }))}
+                      style={[styles.input, (errors.email !== '' || (attemptedNext && !userData.email.trim())) && styles.inputError]}
+                      onChangeText={text => {
+                          setUserData(prevState => ({ ...prevState, email: text }));
+                          setErrors(prevState => ({ ...prevState, email: validateEmail(text) }));
+                      }}
                       value={userData.email}
                       placeholder="Enter your email address"
                   />
               </View>
+
+              {errors.firstName !== '' && <Text style={styles.ErrorText}>{errors.firstName.replace(/\n/g, '\n')}</Text>}
+              {errors.lastName !== '' && <Text style={styles.ErrorText}>{errors.lastName.replace(/\n/g, '\n')}</Text>}
+              {errors.email !== '' && <Text style={styles.ErrorText}>{errors.email}</Text>}
+              {errorMessage !== '' && <Text style={styles.ErrorText}>{errorMessage}</Text>}
 
               <View style={styles.buttonContainer}>
                   <TouchableOpacity style={styles.Loginbutton} onPress={() => handleNextStep()}>
@@ -159,37 +334,52 @@ export default function RegisterScreen() {
           <View style={styles.textContainer}>
               <Text style={styles.text}>Register</Text>
 
+                
               <View style={styles.inputContainer}>
                   <Text style={styles.label}>Username</Text>
                   <TextInput
-                      style={styles.input}
-                      onChangeText={text => setUserData(prevState => ({ ...prevState, username: text }))}
-                      value={userData.username}
-                      placeholder="Enter your username"
-                  />
+                        style={[styles.input, errors.username !== '' && styles.inputError]}
+                        onChangeText={text => {
+                            setUserData(prevState => ({ ...prevState, username: text }));
+                            setErrors(prevState => ({ ...prevState, username: validateUsername(text) }));
+                        }}
+                        value={userData.username}
+                        placeholder="Enter your username"
+                        />
               </View>
 
               <View style={styles.inputContainer}>
                   <Text style={styles.label}>Password</Text>
                   <TextInput
-                      style={styles.input}
-                      onChangeText={text => setUserData(prevState => ({ ...prevState, password: text }))}
-                      value={userData.password}
-                      placeholder="Enter your password"
-                      secureTextEntry={true}
-                  />
+                      style={[styles.input, errors.password !== '' && styles.inputError]}
+                      onChangeText={text => {
+                          setUserData(prevState => ({ ...prevState, password: text }));
+                          setErrors(prevState => ({ ...prevState, password: validatePassword(text), passwordsMatchError: passwordMatch(text, userData.confirmPassword) }));
+                        }}
+                        value={userData.password}
+                        placeholder="Enter your password"
+                        secureTextEntry={true}
+                        />
               </View>
 
               <View style={styles.inputContainer}>
                   <Text style={styles.label}>Confirm Password</Text>
                   <TextInput
-                      style={styles.input}
-                      onChangeText={text => setUserData(prevState => ({ ...prevState, confirmPassword: text }))}
-                      value={userData.confirmPassword}
-                      placeholder="Confirm your password"
-                      secureTextEntry={true}
-                  />
+                      style={[styles.input, errors.confirmPassword !== '' && styles.inputError]}
+                      onChangeText={text => {
+                          setUserData(prevState => ({ ...prevState, confirmPassword: text }));
+                          setErrors(prevState => ({ ...prevState, confirmPassword: validateConfirmPassword(text), passwordsMatchError: passwordMatch(userData.password, text) }));
+                        }}
+                        value={userData.confirmPassword}
+                        placeholder="Confirm your password"
+                        secureTextEntry={true}
+                    />
               </View>
+
+              {errors.username !== '' && <Text style={styles.ErrorText}>{errors.username.replace(/\n/g, '\n')}</Text>}
+              {errors.password !== '' && <Text style={styles.ErrorText}>{errors.password.replace(/\n/g, '\n')}</Text>}
+              {errors.confirmPassword !== '' && <Text style={styles.ErrorText}>{errors.confirmPassword}</Text>}
+              {errors.passwordsMatchError !== '' && <Text style={styles.ErrorText}>{errors.passwordsMatchError}</Text>}
 
               <View style={styles.buttonContainer}>
                   <TouchableOpacity style={styles.Loginbutton} onPress={handleRegister}>
@@ -221,5 +411,4 @@ export default function RegisterScreen() {
 
     </View>
   );
-    
 }
