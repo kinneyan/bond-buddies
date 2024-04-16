1f13d7c0ae1a96da417aba8c813f259a957fddb9
TargetAds <157516049+TargetAds@users.noreply.github.com>
2024-04-16 16:45:04 -0400

    Update FriendshipPage.js

--- frontend/src/pages/FriendshipPage.js
+++ frontend/src/pages/FriendshipPage.js
@@ -8,7 +8,7 @@ const app_name = "bondbuddies.com/"
 function buildPath(route)
 {
     if(process.env.NODE_ENV === 'production')
-        return "https://" + app_name + route;
+        return "httpss://" + app_name + route;
     else
         return "http://localhost:3001/" + route;
 }
