489c1552b6d19cd4cd087d26c8a647c37ad7aa60
yasminie <99419128+yasminie@users.noreply.github.com>
2024-04-16 12:43:07 -0400

    web (register, login, forgot password)

---
+++ LoginPage.js
@@ -0,0 +1,20 @@
+import React, { } from 'react';
+import AuthHeader from '../components/AuthHeader';
+import Footer from '../components/footer';
+import Login from '../components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/LoginPage.css'; 
+
+
+const LoginPage = () => {
+
+  return (
+    <>
+      <AuthHeader />
+      <Login />
+      <Footer />
+    </>
+  );
+};
+
+export default LoginPage;
